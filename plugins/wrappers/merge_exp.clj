d29931371f87cf3bd72348d79c6dfd80ca502386
YJC <yjcyxky@163.com>
2021-03-01 14:55:55 +0800

    Fix Bug: id column name is different in ballgown and count files.

--- plugins/wrappers/merge_exp.clj
+++ plugins/wrappers/merge_exp.clj
@@ -4,33 +4,29 @@
 
 (set! *warn-on-reflection* true)
 
-(defn sort-exp-data
-  [coll]
-  (sort-by :GENE_ID coll))
-
-(defn read-csvs
-  [files]
-  (map #(sort-exp-data (read-csv %)) files))
-
-(defn reorder
-  [data]
-  (let [cols (vec (sort (keys (first data))))]
-    (cons :GENE_ID (vec-remove (.indexOf cols :GENE_ID) cols))))
-
 (defn merge-exp
   "[[{:GENE_ID 'XXX0' :YYY0 1.2} {:GENE_ID 'XXX1' :YYY1 1.3}]
     [{:GENE_ID 'XXX0' :YYY2 1.2} {:GENE_ID 'XXX1' :YYY3 1.3}]]"
   [all-exp-data]
   (apply map merge all-exp-data))
 
+(defn read-csvs
+  [files id]
+  (map #(sort-by id (read-csv %)) files))
+
+(defn reorder
+  [data id]
+  (let [cols (vec (sort (keys (first data))))]
+    (cons id (vec-remove (.indexOf cols id) cols))))
+
 (defn write-csv-by-ordered-cols!
-  [path row-data]
-  (let [cols (reorder row-data)]
+  [path row-data id]
+  (let [cols (reorder row-data id)]
     (write-csv-by-cols! path row-data cols)))
 
 (defn merge-exp-files!
-  "Assumption: all files have the same GENE_ID list, no matter what order."
-  [files path]
-  (->> (read-csvs files)
+  "Assumption: all files have the same `id` column, no matter what order."
+  [files path id]
+  (->> (read-csvs files id)
        (merge-exp)
-       (write-csv-by-ordered-cols! path)))
+       (write-csv-by-ordered-cols! path id)))
